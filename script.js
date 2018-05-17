bb2d2b6c934fa19232a29074c941e6d0a789582a
weiffert <weiffert@gmail.com>
2018-05-16 21:55:54 -0400

    Clear value

--- script.js
+++ script.js
@@ -4,6 +4,9 @@ const celsius = document.querySelector('input[name="celsius"]');
 fahrenheit.addEventListener('change', handleInput);
 celsius.addEventListener('change', handleInput);
 
+fahrenheit.addEventListener('click', clearInput);
+celsius.addEventListener('click', clearInput);
+
 function convertToFahrenheit(value) {
     return value * 9 / 5 + 32;
 }
@@ -18,4 +21,8 @@ function handleInput(event) {
         celsius.value = convertToCelsius(input.value);
     else
         fahrenheit.value = convertToFahrenheit(input.value);
+}
+
+function clearInput() {
+    event.target.value = '';
 }
