02c36bc1c64718b6cb69b2e53e9f07cb1fcac286
weiffert <weiffert@gmail.com>
2018-05-16 21:50:45 -0400

    Basic conversion

---
+++ script.js
@@ -0,0 +1,21 @@
+const fahrenheit = document.querySelector('input[name="fahrenheit"]');
+const celsius = document.querySelector('input[name="celsius"]');
+
+fahrenheit.addEventListener('change', handleInput);
+celsius.addEventListener('change', handleInput);
+
+function convertToFahrenheit(value) {
+    return value * 9 / 5 + 32;
+}
+
+function convertToCelsius(value) {
+    return (value - 32) * 5 / 9;
+}
+
+function handleInput(event) {
+    const input = event.target;
+    if(input.name === 'fahrenheit')
+        celsius.value = convertToCelsius(input.value);
+    else
+        fahrenheit.value = convertToFahrenheit(input.value);
+}
