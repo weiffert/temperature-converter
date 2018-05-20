54b8027ed169c7521e4e9a07d52f4eb957d255ab
weiffert <weiffert@gmail.com>
2018-05-20 18:06:51 -0400

    Set to 32 degrees fahrenheit on load

--- script.js
+++ script.js
@@ -49,19 +49,24 @@ const app = {
     updateCSS() {
         const body = document.querySelector('body');
         let color = [180, 240];
-        if (this.fahrenheit.value != '') {
-            let diff = (parseInt(this.fahrenheit.value) - 60) * 2;
-            color[0] += diff;
-            color[1] -= diff;
-            color = color.map(item => {
-                if (item < 0)
-                    return 0;
-                else if (item > 255)
-                    return 255;
-                else
-                    return Math.floor(item);
-            });
+        let temperature = [];
+        if (this.fahrenheit.value === '') {
+            temperature.push(32);
+        } else {
+            temperature.push(this.fahrenheit.value);
         }
+        
+        let diff = (parseInt(temperature[0]) - 60) * 2;
+        color[0] += diff;
+        color[1] -= diff;
+        color = color.map(item => {
+            if (item < 0)
+                return 0;
+            else if (item > 255)
+                return 255;
+            else
+                return Math.floor(item);
+        });
 
         body.style.backgroundColor = `rgb(${color[0]}, 0, ${color[1]})`;
     },
